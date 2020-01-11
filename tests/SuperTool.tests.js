faf4778eaeeff66077ac74f1318d20ba29059f6e
Jan Küster <jkuester@uni-bremen.de>
2020-01-11 12:49:35 +0100

    example code and example tests added

---
+++ tests/SuperTool.tests.js
@@ -0,0 +1,80 @@
+import { describe, it } from 'mocha'
+import { expect } from 'chai'
+import SuperTool from '../lib/SuperTool'
+
+describe('SuperTool', function () {
+
+  let tool
+
+  beforeEach(function () {
+    tool = new SuperTool()
+  })
+
+  afterEach(function () {
+    tool = null
+  })
+
+  describe('validateState (static)', function () {
+    it('throws if the value is not a valid state', function () {
+      const expectThrow = value => {
+        const fn = () => SuperTool.validateState(value)
+        expect(fn).to.throw(`${value}`)
+      }
+      expectThrow()
+      expectThrow(null)
+      expectThrow(undefined)
+      expectThrow('foo')
+      expectThrow({})
+      expectThrow([])
+      expectThrow(1)
+      expectThrow(true)
+    })
+
+    it('returns nothing on a valid state', function () {
+      const expectValid = value => expect(SuperTool.validateState(value)).to.be.undefined
+      expectValid(SuperTool.states.great)
+      expectValid(SuperTool.states.awesome)
+      expectValid(SuperTool.states.cool)
+      expectValid(SuperTool.states.swag)
+    })
+  })
+
+  describe('constructor', function () {
+    it('is created with default state', function () {
+      expect(tool.state()).to.equal(SuperTool.states.great)
+    })
+
+    it('can be created with an explicit initial state', function () {
+      const toolExplicit = new SuperTool({ state: SuperTool.states.awesome })
+      expect(toolExplicit.state()).to.equal(SuperTool.states.awesome)
+    })
+  })
+
+  describe('state', function () {
+    it('returns the current state if no value is given', function () {
+      // before call
+      expect(tool._state).to.equal(SuperTool.states.great)
+      // call
+      expect(tool.state()).to.equal(SuperTool.states.great)
+      // after call
+      expect(tool._state).to.equal(SuperTool.states.great)
+    })
+
+    it('sets the current state if the state is a valid state value', function () {
+      // before call
+      expect(tool._state).to.equal(SuperTool.states.great)
+      // call
+      expect(tool.state(SuperTool.states.awesome)).to.equal(SuperTool.states.awesome)
+      // after call
+      expect(tool._state).to.equal(SuperTool.states.awesome)
+    })
+  })
+
+  describe('execute', function () {
+    it('executes the current state', function () {
+      expect(tool.execute()).to.equal(`I feel ${SuperTool.states.great}!`)
+      tool.state(SuperTool.states.swag)
+      expect(tool.execute()).to.equal(`I feel ${SuperTool.states.swag}!`)
+    })
+  })
+})
---
+++ tests/utils.tests.js
@@ -0,0 +1,26 @@
+import { describe, it } from 'mocha'
+import { expect } from 'chai'
+import { exists } from '../lib/utils'
+
+describe('exists', function () {
+  it('returns true if the given parameter is defined', function () {
+    const expectTrue = value => expect(exists(value)).to.equal(true)
+    expectTrue('')
+    expectTrue(0)
+    expectTrue(false)
+    expectTrue({})
+    expectTrue([])
+    expectTrue(new Date(0))
+  })
+
+  it('returns false if the given parameter is undefined or null', function () {
+    const expectFalse = value => expect(exists(value)).to.equal(false)
+    expectFalse()
+    expectFalse((void 0))
+    expectFalse(undefined)
+    expectFalse(null)
+
+    let someVar
+    expectFalse(someVar)
+  })
+})
